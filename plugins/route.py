c0d05475fb6500a182f99ffe48bf62d963e44a1d
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-02-21 11:15:09 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -1,7 +1,7 @@
-from aiohttp import web
+# from aiohttp import web
 
-routes = web.RouteTableDef()
+# routes = web.RouteTableDef()
 
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("TechMagazineYT")
+# @routes.get("/", allow_head=True)
+# async def root_route_handler(request):
+#     return web.json_response("TechMagazineYT")
